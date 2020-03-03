13d103099e35e694194d6f416e64a1f276d138d4
naderalfakesh <nader.alfakesh@gmail.com>
2020-03-04 01:22:42 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>{"Actors Page"}</h1>
+      {
+        actors.map((x)=><div key={Math.random()} className="actor">
+          <h1>{x.name}</h1>
+          <ul>
+           <li>{x.movies[0]}</li>
+           <li>{x.movies[1]}</li>
+           <li>{x.movies[2]}</li>
+          </ul>
+        </div>)
+      }
     </div>
   );
 };
